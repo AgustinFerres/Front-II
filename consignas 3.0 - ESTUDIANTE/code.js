a7fd227345caec5004e40add73f5f2e56ee36a07
AgustinFerres <agustinferres4403@gmail.com>
2022-08-21 13:59:47 -0300

    parcial

---
+++ consignas 3.0 - ESTUDIANTE/code.js
@@ -0,0 +1,99 @@
+/* --------------------------- NO TOCAR DESDE ACÁ --------------------------- */
+
+let datosPersona = {
+  nombre: "",
+  edad: 0,
+  ciudad: "",
+  interesPorJs: "",
+};
+
+const listado = [{
+    imgUrl: "https://huguidugui.files.wordpress.com/2015/03/html1.png",
+    lenguajes: "HTML y CSS",
+    bimestre: "1er bimestre",
+  },
+  {
+    imgUrl: "https://jherax.files.wordpress.com/2018/08/javascript_logo.png",
+    lenguajes: "Javascript",
+    bimestre: "2do bimestre",
+  },
+  {
+    imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png",
+    lenguajes: "React JS",
+    bimestre: "3er bimestre",
+  },
+];
+
+const profileBtn = document.querySelector("#completar-perfil");
+const materiasBtn = document.querySelector("#obtener-materias");
+const verMasBtn = document.querySelector("#ver-mas");
+const cambiarTema = document.querySelector('#cambiar-tema');
+
+profileBtn.addEventListener("click", renderizarDatosUsuario);
+materiasBtn.addEventListener("click", recorrerListadoYRenderizarTarjetas);
+cambiarTema.addEventListener("click", alternarColorTema);
+/* --------------------------- NO TOCAR HASTA ACÁ --------------------------- */
+
+function obtenerDatosDelUsuario() {
+  /* --------------- PUNTO 1: Escribe tu codigo a partir de aqui --------------- */
+
+  datosPersona.nombre = prompt('Ingresa tu nombre');
+
+  datosPersona.edad = (2022 - Number(prompt('Ingresa tu año de nacimiento')));
+
+  datosPersona.ciudad = prompt('Ingresa tu ciudad');
+
+  datosPersona.interesPorJs = (confirm('Te interesa java') ? 'Si' : 'No'); 
+
+}
+
+function renderizarDatosUsuario() {
+  /* ------------------- NO TOCAR NI ELIMINAR ESTA FUNCION. ------------------- */
+  obtenerDatosDelUsuario();
+  /* --------------- PUNTO 2: Escribe tu codigo a partir de aqui --------------- */
+  
+  const contenedor = document.querySelector('.card-header')
+
+  contenedor.innerHTML = `
+  <h3>Nombre: <span id="nombre">${datosPersona.nombre}</span></h3>
+  <h3>Edad: <span id="edad">${datosPersona.edad}</span></h3>
+  <h3>Ciudad: <span id="ciudad">${datosPersona.ciudad}</span></h3>
+  <h3>Interes en Javascript: <span id="javascript">${datosPersona.interesPorJs}</span></h3>
+  `;
+}
+
+
+function recorrerListadoYRenderizarTarjetas() {
+  /* ------------------ PUNTO 3: Escribe tu codigo desde aqui ------------------ */
+  const contenedor = document.querySelector('#fila');
+
+  contenedor.innerHTML = ``;
+
+  listado.forEach(materia =>{
+    contenedor.innerHTML += `
+      <article class='caja' >
+        <img src='${materia.imgUrl}' alt='${materia.lenguajes}'/>
+        <p class='lenguajes'> Lenguajes: ${materia.lenguajes}</p>
+        <p class='bimestre'> Bimestre: ${materia.bimestre}</p>
+      </article>
+    `;
+  });
+}
+
+function alternarColorTema() {
+  /* --------------------- PUNTO 4: Escribe tu codigo aqui --------------------- */
+  const sitio = document.querySelector('#sitio');
+
+  sitio.classList.toggle('dark')
+}
+
+/* --------------------- PUNTO 5: Escribe tu codigo aqui --------------------- */
+
+document.addEventListener('keypress', function(e){
+  
+  const parrafo = document.querySelector('#sobre-mi');
+
+  if(e.key === 'f'){
+    parrafo.classList.remove('oculto');
+  }
+})
